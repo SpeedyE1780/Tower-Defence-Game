0dd418114c8584aedd3e16b0f346c73a5636e14d
SpeedyE1780 <sandroesaab@gmail.com>
2021-07-07 09:12:09 +0300

    Replaced ongui with scene text for fps display

--- Assets/Scripts/Utilities/FPSDisplay.cs
+++ Assets/Scripts/Utilities/FPSDisplay.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class FPSDisplay : MonoBehaviour
 {
-    float deltaTime = 0.0f;
     [SerializeField] private int targetRate;
+    [SerializeField] private Text fpsText;
+    float deltaTime = 0.0f;
+    string text;
+    float fps;
+    float msec;
+
 
     private void Awake()
     {
@@ -13,21 +19,9 @@ public class FPSDisplay : MonoBehaviour
     void Update()
     {
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-    }
-
-    void OnGUI()
-    {
-        int w = Screen.width, h = Screen.height;
-
-        GUIStyle style = new GUIStyle();
-
-        Rect rect = new Rect(0, 0, w, h * 2 / 100);
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 100;
-        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
-        float msec = deltaTime * 1000.0f;
-        float fps = 1.0f / deltaTime;
-        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-        GUI.Label(rect, text, style);
+        msec = deltaTime * 1000.0f;
+        fps = 1.0f / deltaTime;
+        text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        fpsText.text = text;
     }
 }
