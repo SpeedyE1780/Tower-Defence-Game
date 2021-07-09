80e83b11beae1a16905aaac4c098cbf20e35a3be
SpeedyE1780 <sandroesaab@gmail.com>
2021-07-09 11:31:12 +0300

    Added overlapspherenonalloc to test scene

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class TestScript : MonoBehaviour
+{
+    Collider[] inRange;
+
+    void Start()
+    {
+        inRange = new Collider[5];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Overlap();
+        }
+    }
+
+    private void Overlap()
+    {
+        Profiler.BeginSample("OVerlap");
+        for (int i = 0; i < inRange.Length; i++)
+            inRange[i] = null;
+
+        Physics.OverlapSphereNonAlloc(transform.position, 10f, inRange);
+        Profiler.EndSample();
+    }
+}
