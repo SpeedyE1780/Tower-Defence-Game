89b3ca00a148d1fd9b77e8c41c2bbc86ca21131f
SpeedyE1780 <sandroesaab@gmail.com>
2021-07-12 10:42:02 +0300

    Added music keys class

---
+++ Assets/BrightLab-Tools/AudioManager/Scripts/MusicKeys.cs
@@ -0,0 +1,10 @@
+namespace AudioManager
+{
+    public static class MusicKeys
+    {
+        public const string MusicMutedKey = "MusicMuted";
+        public const string MusicVolumeKey = "MusicVolume";
+        public const string SFXMutedKey = "SFXMuted";
+        public const string SFXVolumeKey = "SFXVolume";
+    }
+}
