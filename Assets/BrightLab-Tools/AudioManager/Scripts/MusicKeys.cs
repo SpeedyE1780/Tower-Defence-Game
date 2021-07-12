8b1c719c69e1f1efffade8c2bdd699914a6cd410
SpeedyE1780 <sandroesaab@gmail.com>
2021-07-12 10:55:14 +0300

    Added Master Volume Keys

--- Assets/BrightLab-Tools/AudioManager/Scripts/MusicKeys.cs
+++ Assets/BrightLab-Tools/AudioManager/Scripts/MusicKeys.cs
@@ -2,6 +2,8 @@ namespace AudioManager
 {
     public static class MusicKeys
     {
+        public const string MasterMutedKey = "MasterMuted";
+        public const string MasterVolumeKey = "MasterVolume";
         public const string MusicMutedKey = "MusicMuted";
         public const string MusicVolumeKey = "MusicVolume";
         public const string SFXMutedKey = "SFXMuted";
