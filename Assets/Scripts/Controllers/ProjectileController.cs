81283857ee01c9bb3e531fc4fc9ae46c1dbc92aa
SpeedyE1780 <sandroesaab@gmail.com>
2021-07-16 14:59:10 +0300

    Optimized projectile script

--- Assets/Scripts/Controllers/ProjectileController.cs
+++ Assets/Scripts/Controllers/ProjectileController.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
+    private const string PlayerTag = "Player";
+
     [SerializeField] private PoolID id;
     [SerializeField] private float lifeTime;
     [SerializeField] private float speed;
     Rigidbody rb;
+    float currentLifetime;
 
     void Start() => rb = GetComponent<Rigidbody>();
     private void OnEnable() => StartCoroutine(PoolObject());
@@ -14,14 +17,19 @@ public class ProjectileController : MonoBehaviour
 
     IEnumerator PoolObject()
     {
-        yield return new WaitForSeconds(lifeTime);
+        currentLifetime = 0;
+        while (currentLifetime < lifeTime)
+        {
+            currentLifetime += Time.deltaTime;
+            yield return null;
+        }
         if (gameObject.activeSelf)
             PoolManager.Instance.AddToPool(id, gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag(PlayerTag))
             PoolManager.Instance.AddToPool(id, gameObject);
     }
 }
