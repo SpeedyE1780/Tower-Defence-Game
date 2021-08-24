a0dd4a9c091b4b032e29d028d06fac32c2ec0ce7
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2021-08-24 15:12:19 +0300

    Fixed health bar being inverted for enemies

--- Assets/Scripts/Utilities/LookAtTarget.cs
+++ Assets/Scripts/Utilities/LookAtTarget.cs
@@ -5,8 +5,6 @@ public class LookAtTarget : MonoBehaviour
     private static Transform lookAtTarget;
     [RuntimeInitializeOnLoadMethod]
     public static void SetLookAt() => lookAtTarget = Camera.main.transform;
-    Quaternion originalRotation;
 
-    private void Awake() => originalRotation = transform.rotation;
-    private void LateUpdate() => transform.rotation = lookAtTarget.rotation * originalRotation;
+    private void LateUpdate() => transform.rotation = lookAtTarget.rotation;
 }
