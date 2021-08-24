ca6cd04b93d0142ba9f0cda7bef10a12de97fb0a
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2021-08-24 11:09:55 +0300

    Move detection job to its own script

---
+++ Assets/Scripts/Jobs/DetectionJob.cs
@@ -0,0 +1,44 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+[BurstCompile]
+public struct DetectionJob : IJobParallelFor
+{
+    public NativeArray<UnitInfo> unitInfo;
+    [ReadOnly] public NativeArray<UnitInfo> enemyInfo;
+
+    public void Execute(int index)
+    {
+        //Get current unit info and position
+        UnitInfo currentUnit = unitInfo[index];
+        float3 position = currentUnit.Position;
+        float currentDistance = math.INFINITY;
+
+        foreach (UnitInfo enemy in enemyInfo)
+        {
+            //Calculate distance from unit
+            float3 enemyPosition = enemy.Position;
+            float temp = math.distance(position, enemyPosition);
+
+            //Update target and current distance
+            if (temp < currentDistance)
+            {
+                currentUnit.TargetID = enemy.InstanceID;
+                currentDistance = temp;
+            }
+        }
+
+        //Update unit info inside dictionary
+        unitInfo[index] = currentUnit;
+    }
+}
+
+//Struct used to store info inside jobs
+public struct UnitInfo
+{
+    public int InstanceID;
+    public float3 Position;
+    public int TargetID;
+}
