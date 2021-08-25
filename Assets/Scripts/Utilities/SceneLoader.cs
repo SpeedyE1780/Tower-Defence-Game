ea4b40ebf3e27231357c1205a32e9ea3aea33b8c
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2021-08-25 18:04:38 +0300

    Added scene transition

--- Assets/Scripts/Utilities/SceneLoader.cs
+++ Assets/Scripts/Utilities/SceneLoader.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(int scene) => SceneManager.LoadScene(scene);
+    public void LoadScene(int scene) => SceneTransitionManager.Instance.StartSceneLoading(scene);
 }
---
+++ Assets/Scripts/Utilities/SceneTransitionManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : Singleton<SceneTransitionManager>
+{
+    [SerializeField] CanvasGroup group;
+    [SerializeField] Canvas canvas;
+
+
+    protected override void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+        canvas.enabled = false;
+    }
+
+    public void StartSceneLoading(int sceneIndex)
+    {
+        canvas.enabled = true;
+        StartCoroutine(LoadScene(sceneIndex));
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        yield return StartCoroutine(FadeScene(0, 1));
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneIndex);
+        yield return new WaitUntil(() => loadOperation.isDone);
+        yield return StartCoroutine(FadeScene(1, 0));
+        canvas.enabled = false;
+    }
+
+    IEnumerator FadeScene(float start, float end)
+    {
+        float timer = 0;
+        float duration = 0.5f;
+        while (timer < duration)
+        {
+            yield return null;
+            timer += Time.deltaTime;
+            group.alpha = Mathf.Lerp(start, end, timer / duration);
+        }
+    }
+}
