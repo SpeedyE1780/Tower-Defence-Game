bc2af913d0e3347888759e910f6b618b17491d2b
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2021-08-26 13:06:02 +0300

    Added AOE Infantry unit

---
+++ Assets/Scripts/Controllers/Units/AOEInfantry.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AOEInfantry : InfantryController
+{
+    [SerializeField] private float range;
+
+    protected override void ApplyDamage()
+    {
+        AOEManager.Instance.ApplyAOEDamage(currentTarget.transform.position, range, damage, unitMask);
+    }
+}
