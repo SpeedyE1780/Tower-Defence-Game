beeac556e47ee3da68a287fca3cac2d055c07801
SpeedyE1780 <79991480+SpeedyE1780@users.noreply.github.com>
2021-08-30 13:13:49 +0300

    Added mana bar for healer

--- Assets/Scripts/Controllers/Units/HealerController.cs
+++ Assets/Scripts/Controllers/Units/HealerController.cs
@@ -1,19 +1,52 @@
 using UnityEngine;
+using Slider = UnityEngine.UI.Slider;
 
 public class HealerController : RangedController
 {
     [SerializeField] protected int healAmount;
+    [SerializeField] protected int healMana;
+    [SerializeField] private Slider manaBar;
+    private int usedMana;
+
+    private bool CanHeal => usedMana <= healMana;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        manaBar.gameObject.SetActive(true);
+        EventManager.OnWaveEnded += ResetCharges;
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        manaBar.gameObject.SetActive(false);
+        EventManager.OnWaveEnded -= ResetCharges;
+    }
 
     protected override void Shoot()
     {
-        if (!CanAttack)
+        if (!CanAttack || !CanHeal)
             return;
 
         //Heal target & set to null to find a new target the next frame
         Heal();
         currentTarget = null;
 
+        UpdateManaBar();
         ResetAttackCooldown();
+    }
+
+    private void UpdateManaBar()
+    {
+        usedMana++;
+        manaBar.value = 1 - (float)usedMana / healMana;
+    }
+
+    private void ResetCharges()
+    {
+        usedMana = 0;
+        manaBar.value = 1;
     }
 
     protected virtual void Heal() => currentTarget.Heal(healAmount);
